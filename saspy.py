2908662572e260368c23e400868e11f4d3d26782
WasfiS <50295773+WasfiS@users.noreply.github.com>
2023-02-25 14:02:04 +0200

    Initial commit

---
+++ saspy.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+from sas7bdat import SAS7BDAT
+
+# import platform
+# print('Python: ', platform.python_version())
+# print('pandas: ', pd.__version__)
+# print('pyarrow: ', pa.__version__)
+
+with SAS7BDAT('/Users/wasfisamodien/Downloads/pss1718_pu_sas7bdat/pss1718_pu.sas7bdat', skip_header=False) as reader:
+    df = reader.to_data_frame()
+
+df.info(verbose=True)
+print(df.head())
+
+with SAS7BDAT('/Users/wasfisamodien/Downloads/pss1718_pu_sas7bdat/pss1718_pu.sas7bdat', skip_header=False) as reader:
+    reader.convert_file('/Users/wasfisamodien/Downloads/pss1718_pu_sas7bdat/pss1718_pu.csv', delimiter=',')
+
